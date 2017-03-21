d795b030f9b6e798476cb70799ca02a54604c3a2
Abdussamed Bilginer <abdussamedbilginer@gmail.com>
2017-03-21 21:39:49 +0100

    Structure Finished, Connection to Server must be completed

---
+++ app/src/main/java/com/example/abdussamed/loginregistration/activity/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.abdussamed.loginregistration.activity;
+
+import com.example.abdussamed.loginregistration.R;
+import com.example.abdussamed.loginregistration.helper.SQLiteHandler;
+import com.example.abdussamed.loginregistration.helper.SessionManager;
+
+import java.util.HashMap;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ *
+ * Here we are just fetching the logged user information from SQLite and displaying it on the
+ * screen. The logout button will logout the user by clearing the session and deleting the user
+ * from SQLite table.
+ */
+public class MainActivity extends Activity {
+
+    private TextView txtName;
+    private TextView txtEmail;
+    private Button btnLogout;
+
+    private SQLiteHandler db;
+    private SessionManager sessionManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtName = (TextView) findViewById(R.id.name);
+        txtEmail = (TextView) findViewById(R.id.email);
+        btnLogout = (Button) findViewById(R.id.btnLogout);
+
+        // SqLite database handler
+        db = new SQLiteHandler(getApplicationContext());
+
+        // session manager
+        sessionManager = new SessionManager(getApplicationContext());
+
+        if (!sessionManager.isLoggedIn()) {
+            logoutUser();
+        }
+
+        // Fetching user details from sqlite
+        HashMap<String, String> user = db.getUserDetails();
+
+        String name = user.get("name");
+        String email = user.get("email");
+
+        // Displaying the user details on the screen
+        txtName.setText(name);
+        txtEmail.setText(email);
+
+        // Logout button click event
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                logoutUser();
+            }
+        });
+    }
+    /**
+     * Logging out the user. Will set isLoggedIn flag to false in shared
+     * preferences Clears the user data from sqlite users table
+     * */
+    private void logoutUser() {
+        sessionManager.setLogin(false);
+
+        db.deleteUsers();
+
+        // Launching the login activity
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
+
---
+++ app/src/main/java/com/example/abdussamed/loginregistration/app/AppConfig.java
@@ -0,0 +1,17 @@
+package com.example.abdussamed.loginregistration.app;
+
+/**
+ *
+ * In this class we
+ * declare the login and registration urls. While testing you need to replace the ip address with
+ * your localhost pc ip.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.178.24/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.178.24/android_login_api/register.php";
+}
+
